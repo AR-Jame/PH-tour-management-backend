5a1e5a469e8e0caa0f8e6dfaec14c5d978b274d3
AR-Jame <ar.jame333@gmail.com>
2025-07-11 10:23:51 +0600

    Global err handler and not found route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors'
 import { router } from './routes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app = express();
 
@@ -10,8 +12,13 @@ app.use(cors());
 app.use("/api/v1", router);
 
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Alhamdulillah, app is running')
 })
+
+
+app.use(globalErrorHandler)
+
+app.use(notFound)
 
 export default app
