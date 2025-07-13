9893f2700e67e8232def0176f468d68ff3e471ea
AR-Jame <ar.jame333@gmail.com>
2025-07-13 22:40:18 +0600

    All user get and password hashed

--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -1,18 +1,43 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelper/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import StatusCodes from 'http-status-codes';
+import bcryptjs from 'bcryptjs';
 
 const createUser = async (payload: Partial<IUser>) => {
-    const { name, email } = payload;
+    const { email, password, ...rest } = payload;
+
+    const isUserExist = await User.findOne({ email: email });
+
+    if (isUserExist) {
+        throw new AppError(StatusCodes.BAD_REQUEST, "Email already exists.")
+    }
+
+
+    const hashedPassword = await bcryptjs.hash(password as string, 10);
+
+    const authProvider: IAuthProvider = {
+        provider: "credentials",
+        providerId: email as string
+    }
 
     const user = await User.create({
-        name,
-        email
+        email,
+        password: hashedPassword,
+        auths: [authProvider],
+        ...rest
     });
     return user
+}
+
+const getAllUser = async () => {
+    const users = await User.find({});
+    return users
 }
 
 
 
 export const userServices = {
-    createUser
+    createUser,
+    getAllUser
 }
